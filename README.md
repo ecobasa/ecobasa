e9c7883b39919fb87b7018e31855c228d874f90b
marmorkuchen <marmorkuchen@kodeaffe.de>
2013-06-21 15:21:31 +0200

    added pyscopg2, South and enabled /admin

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 ecobasa_uanoa
 =============
 
-relaunch of ecobasa.org
+Relaunch of http://ecobasa.org
+
+
+Database
+--------
+
+We use PostgreSQL: http://www.postgresql.org/.
+See http://od-eon.com/blogs/calvin/postgresql-cheat-sheet-beginners/
+for a bit of setup info. Basically:
+	$ sudo su - postgres
+	$ createuser django -P # enter password, answer no to all questions
+	$ createdb -E utf8 -O django ecobasa_uanoa -T template0
+
+As user posgres, you might have to check /etc/postgresql/9.1/main/pg_hba.conf
+if the TYPE 'local' for USER 'all' has METHOD set to 'md5' . Afterwards you
+should be able to syncdb as your normal user:
+
+	$ ./manage.py syncdb
--- ecobasa_uanoa/settings.py
+++ ecobasa_uanoa/settings.py
@@ -11,13 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ecobasa_uanoa',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
@@ -120,10 +119,10 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
