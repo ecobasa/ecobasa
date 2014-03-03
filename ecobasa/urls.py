49b08a054c13da8b8200eb068116f5a6eea5c207
Sebastian Henschel <sh@sinnwerkstatt.com>
2014-03-03 21:41:17 +0100

    Refers #2 -- Added serving of media files to ecobasa.urls

--- ecobasa/urls.py
+++ ecobasa/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
@@ -12,6 +13,8 @@ urlpatterns = patterns('',
     url(r'^accounts/', include('cosinnus.utils.django_auth_urls')),
     url(r'^accounts/', include('userprofiles.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
 
     # url(r'^skillshare/', include('skillshare.urls', namespace='skillshare')),
     # url(r'^references/', include('references.urls', namespace='references')),
