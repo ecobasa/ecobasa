dfdb52acf8a08033acdc4b065e0f2dde9cc89088
Arne Bollinger <ab@sinnwerkstatt.com>
2014-03-26 16:50:34 +0100

    new awesome footer from attila

--- ecobasa/static/js/main.js
+++ ecobasa/static/js/main.js
@@ -2,11 +2,15 @@
    if( ! $('#myCanvas').tagcanvas({
      textColour : '#000',
      outlineColour : '#da6137',
+     outlineMethod : 'colour',
      outlineThickness : 1,
-     stretchX : 3,
+     pulsateTime : '1',
+     stretchX : 5,
      imageScale : 0.6,
      maxSpeed : 0.03,
-     depth : 0.75
+     depth : 0.5,
+     zoom : 0.75,
+     fadeIn: 2000
    })) {
      // TagCanvas failed to load
      $('#myCanvasContainer').hide();
