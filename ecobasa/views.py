d901091b948cf37ad452edcc1645fb5c452945d8
Sebastian Henschel <sh@sinnwerkstatt.com>
2014-03-29 09:41:28 +0100

    Removed crufty views

--- ecobasa/views.py
+++ ecobasa/views.py
@@ -10,32 +10,6 @@ from skillshare import get_skills_for_owner
 from references import get_references_for_receiver
 
 
-
-class HomeView(TemplateView):
-    template_name = 'home.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(HomeView, self).get_context_data(**kwargs)
-        context['users'] = User.objects.filter(is_superuser=False)
-        return context
-
-
-# class ProfileView(DetailView):
-#     model = User
-#     slug_field = 'username'
-#     template_name = 'profile.html'
-
-#     def get_context_data(self, **kwargs):
-#         context = super(ProfileView, self).get_context_data(**kwargs)
-#         return_url = reverse('profile', kwargs={'slug': context['object']})
-#         context['skills'] = get_skills_for_owner(
-#             self.request, context['object'], return_url)
-#         context['references'], context['reference_form'] = \
-#             get_references_for_receiver(self.request, context['object'],
-#             return_url)
-#         return context
-
-
 class EcobasaProfileView(UserProfileDetailView):
     def get_context_data(self, **kwargs):
         context = super(EcobasaProfileView, self).get_context_data(**kwargs)
