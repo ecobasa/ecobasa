577287059d8bb59dab6401f132fc9a069094ac46
Arne Bollinger <ab@sinnwerkstatt.com>
2014-04-04 18:22:41 +0200

    settings fix

--- ecobasa/default_settings.py
+++ ecobasa/default_settings.py
@@ -54,7 +54,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.abspath(os.path.join(PROJECT_ROOT, 'static'))
+STATIC_ROOT = os.path.abspath(os.path.join(PROJECT_ROOT, 'static-collected'))
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -65,6 +65,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -136,7 +137,7 @@ CMS_TEMPLATES = (
 
 LANGUAGES = [
     ('en', _('English')),
-    ('de, _('German')),
+    ('de', _('German')),
 ]
 
 INSTALLED_APPS = (
