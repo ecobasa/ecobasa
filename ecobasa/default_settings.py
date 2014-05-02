f774a35c7cfd52bcf0c116152c954663ad1c576e
Arne Bollinger <ab@sinnwerkstatt.com>
2014-05-02 19:47:47 +0200

    new ecobasa startpage

--- ecobasa/default_settings.py
+++ ecobasa/default_settings.py
@@ -106,8 +106,8 @@ TEMPLATE_CONTEXT_PROCESSORS = [
 THUMBNAIL_PROCESSORS = (
     'easy_thumbnails.processors.colorspace',
     #'easy_thumbnails.processors.autocrop',
-    'easy_thumbnails.processors.scale_and_crop',
-    #'filer.thumbnail_processors.scale_and_crop_with_subject_location',
+    #'easy_thumbnails.processors.scale_and_crop',
+    'filer.thumbnail_processors.scale_and_crop_with_subject_location',
     'easy_thumbnails.processors.filters',
 )
 
