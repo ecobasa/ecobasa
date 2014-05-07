be03f8d6df5b03316aed227ffb1621189947ba5f
Arne Bollinger <ab@sinnwerkstatt.com>
2014-05-07 20:17:44 +0200

    fabfile

--- fabfile.py
+++ fabfile.py
@@ -5,15 +5,15 @@ from fabvenv import virtualenv
 def staging():
     env.hosts = ['ecobasa@server.sinnwerkstatt.com']
     env.path = '/srv/ecobasa.sinnwerkstatt.com/community-tours/'
-    env.virtualenv_path = '/srv/ecobasa.sinnwerkstatt.com/community-toursenv/'
+    env.virtualenv_path = '/srv/ecobasa.sinnwerkstatt.com/ecobasaenv/'
     env.push_branch = 'tour'
     env.push_remote = 'origin'
 
 
 def production():
     env.hosts = ['m1487@community-tours.org']
-    env.path = '~/ecobasa/'
-    env.virtualenv_path = '~/ecobasaenv/'
+    env.path = '~/community-tours/'
+    env.virtualenv_path = '~/community-toursenv/'
     env.push_branch = 'tour'
     env.push_remote = 'origin'
 
