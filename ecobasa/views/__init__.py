6b841cb303c6f3d68d6a51abf246ea318ba7a03f
Sebastian Henschel <sh@sinnwerkstatt.com>
2014-05-07 16:10:54 +0200

    Refers #47 -- Split views into several files

---
+++ ecobasa/views/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from .bus import *  # noqa
+from .community import *  # noqa
+from .find import *  # noqa
+from .organiser import *  # noqa
+from .pioneer import *  # noqa
+from .registration import *  # noqa
---
+++ ecobasa/views/pioneer.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404
+from django.utils.decorators import method_decorator
+from django.utils.translation import ugettext_lazy as _
+from django.views.generic import DetailView, UpdateView
+
+from cosinnus.models.group import MEMBERSHIP_ADMIN
+from cosinnus.views.user import UserListView, USER_MODEL
+
+from ..forms import PioneerProfileForm
+
+
+class PioneerListView(UserListView):
+    template_name = 'ecobasa/pioneer_list.html'
+
+    def get_queryset(self):
+        users = super(PioneerListView, self).get_queryset()
+        pioneers = users.exclude(cosinnus_memberships__status=MEMBERSHIP_ADMIN)
+        pioneers = pioneers.exclude(is_superuser=True, is_staff=True)
+        return pioneers
+
+pioneer_list = PioneerListView.as_view()
+
+
+class PioneerDetailView(DetailView):
+    """
+    cosinnus has a weird requirement to require staff to see a user's details.
+    """
+    model = USER_MODEL
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
+    template_name = 'ecobasa/pioneer_detail.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(PioneerDetailView, self).dispatch(*args, **kwargs)
+
+pioneer_detail = PioneerDetailView.as_view()
+
+
+class PioneerUpdateView(UpdateView):
+    """
+    Alas, cosinnus' UserUpdateView only allows to edit the logged-in user's
+    profile.
+    """
+    form_class = PioneerProfileForm
+    template_name = 'ecobasa/pioneer_form.html'
+
+    def get_object(self):
+        user = get_object_or_404(USER_MODEL, username=self.kwargs['username'])
+        return user.cosinnus_profile
+
+    def dispatch(self, *args, **kwargs):
+        user = self.request.user
+        if user.is_superuser or user.username == self.kwargs['username']:
+            return super(PioneerUpdateView, self).dispatch(*args, **kwargs)
+        else:
+            raise PermissionDenied
+
+    def form_valid(self, form):
+        result = super(PioneerUpdateView, self).form_valid(form)
+        messages.success(self.request,
+            _('The profile was successfully updated.'))
+        return result
+
+    def get_success_url(self):
+        kwargs = {'username': self.object.user.username}
+        return reverse('pioneer-detail', kwargs=kwargs)
+
+pioneer_update = PioneerUpdateView.as_view()
