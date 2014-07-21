efa761dffcf9f601888d078190eebc496b65a9e2
Arne Bollinger <ab@sinnwerkstatt.com>
2014-07-21 22:50:28 +0200

    #78 WIP caravan profiles data doesnt show in template

--- ecobasa/models/caravan.py
+++ ecobasa/models/caravan.py
@@ -1,14 +1,31 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
+from easy_thumbnails.fields import ThumbnailerImageField
+from taggit.managers import TaggableManager
+from taggit.models import TaggedItemBase
 
-from cosinnus.models import CosinnusGroup
+from cosinnus.models import (
+    BaseUserProfile, BaseUserProfileManager, CosinnusGroup)
 
+
+class TaggedOffer(TaggedItemBase):
+    content_object = models.ForeignKey('Caravan')
+
+    class Meta:
+        app_label = 'ecobasa'
 
 @python_2_unicode_compatible
 class Caravan(CosinnusGroup):
+    avatar = ThumbnailerImageField(_('avatar'),
+        upload_to='caravans', null=True, blank=True)
+    offers = TaggableManager(_('offers'),
+        through=TaggedOffer, related_name='_offer', blank=True,
+        help_text='Anything useful for communites that you can take with you on your caravan (clay, tools, music, books..')
+
     class Meta:
         app_label = 'ecobasa'
         ordering = ('name',)
--- ecobasa/views/caravan.py
+++ ecobasa/views/caravan.py
@@ -15,6 +15,12 @@ from cosinnus.views.widget import DashboardMixin
 from ..forms import CaravanForm
 from ..models import Caravan
 
+from cosinnus.models import CosinnusGroup
+from cosinnus.models.group import MEMBERSHIP_ADMIN, MEMBERSHIP_PENDING
+from cosinnus.views.group import GroupListView
+from cosinnus.views.widget import DashboardMixin
+from cosinnus_note.models import Note
+
 
 class CaravanListView(ListView):
     template_name = 'ecobasa/caravan_list.html'
@@ -62,6 +68,10 @@ class CaravanDetailView(GroupDetailView):
 
     def get_filter(self):
         return {'group_id': self.object.pk}
+
+    def get_context_data(self, **kwargs):
+        context = super(CaravanDetailView, self).get_context_data(**kwargs)
+
 
 caravan_detail = CaravanDetailView.as_view()
 
