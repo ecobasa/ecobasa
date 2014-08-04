329b9b71513f83380111429d25a6adfab3e44ce1
Arne Bollinger <ab@sinnwerkstatt.com>
2014-08-04 18:58:48 +0300

    WIP list blog posts in /about timeline

---
+++ ecobasa/views/about.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.utils.decorators import method_decorator
+
+from cosinnus.models import CosinnusGroup
+from cosinnus_note.models import Note
+
+class NoteListView():
+    template_name = 'about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(NoteListView, self).get_context_data(**kwargs)
+        context.update({'posts': Note.objects.all})
+        return context
+
+note_list = NoteListView.as_view()
