b78a32ae0707b78f55316a73f97bb854f3dcc501
Arne Bollinger <ab@sinnwerkstatt.com>
2014-08-04 17:02:54 +0300

    avatar and offers form fields and list entries deleted, link to caravan details in caravan list

--- ecobasa/forms/caravan.py
+++ ecobasa/forms/caravan.py
@@ -11,7 +11,7 @@ from ..models import Caravan
 class _CaravanForm(TagObjectForm):
 
     class Meta:
-        fields = ('avatar', 'name', 'slug', 'description', 'offers', 'public',)
+        fields = ('name', 'slug', 'description', 'public')
         model = Caravan
 
     def __init__(self, *args, **kwargs):
