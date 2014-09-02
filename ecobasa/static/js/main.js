a5cbfd3912be32e94fb1d03b4334b74bb155c411
Arne Bollinger <ab@sinnwerkstatt.com>
2014-09-02 14:13:45 +0200

    main.js restructured

--- ecobasa/static/js/main.js
+++ ecobasa/static/js/main.js
@@ -244,28 +244,30 @@
 
 
 $(document).ready(function() {
-   if( ! $('#myCanvas').tagcanvas({
-     textColour : '#000',
-     outlineColour : '#da6137',
-     outlineMethod : 'colour',
-     outlineThickness : 1,
-     pulsateTime : '1',
-     stretchX : 5,
-     imageScale : 0.6,
-     maxSpeed : 0.03,
-     depth : 0.5,
-     zoom : 0.75,
-     fadeIn: 2000
-   })) {
-     // TagCanvas failed to load
-     $('#myCanvasContainer').hide();
-   }
-
+	var foobar = function() {
+		if( ! $('#myCanvas').tagcanvas({
+		     textColour : '#000',
+		     outlineColour : '#da6137',
+		     outlineMethod : 'colour',
+		     outlineThickness : 1,
+		     pulsateTime : '1',
+		     stretchX : 5,
+		     imageScale : 0.6,
+		     maxSpeed : 0.03,
+		     depth : 0.5,
+		     zoom : 0.75,
+		     fadeIn: 2000
+		   })) {
+		     // TagCanvas failed to load
+		     $('#myCanvasContainer').hide();
+		   }
+	};
+	window.setTimeout(foobar, 1000);
 	$.ecobasa.fullcalendar();
 	$.ecobasa.calendarBig();
 //	$.ecobasa.calendarDayTimeChooser();
-});
 
-$('#bus').tooltip(); 
-$('.container.about #progress_bar').tooltip();
-$('.email').tooltip();
+	$('#bus').tooltip(); 
+	$('.container.about #progress_bar').tooltip();
+	$('.email').tooltip();
+});
