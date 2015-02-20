daf2a03f86bc98c48882d666cefff346723ab04f
Arne Bollinger <ab@sinnwerkstatt.com>
2015-02-20 04:58:02 +0100

    fixed migrations

--- ecobasa/migrations/0016_auto__add_taggedreferenceservice__add_taggedreferenceproduct__add_tagg.py
+++ ecobasa/migrations/0016_auto__add_taggedreferenceservice__add_taggedreferenceproduct__add_tagg.py
@@ -8,13 +8,13 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding model 'TaggedReferenceSkill'
-        db.create_table(u'ecobasa_taggedreferenceskill', (
+        # Adding model 'TaggedReferenceService'
+        db.create_table(u'ecobasa_taggedreferenceservice', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('tag', self.gf('django.db.models.fields.related.ForeignKey')(related_name=u'ecobasa_taggedreferenceskill_items', to=orm['taggit.Tag'])),
+            ('tag', self.gf('django.db.models.fields.related.ForeignKey')(related_name=u'ecobasa_taggedreferenceservice_items', to=orm['taggit.Tag'])),
             ('content_object', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['ecobasa.Reference'])),
         ))
-        db.send_create_signal(u'ecobasa', ['TaggedReferenceSkill'])
+        db.send_create_signal(u'ecobasa', ['TaggedReferenceService'])
 
         # Adding model 'TaggedReferenceProduct'
         db.create_table(u'ecobasa_taggedreferenceproduct', (
@@ -24,13 +24,13 @@ class Migration(SchemaMigration):
         ))
         db.send_create_signal(u'ecobasa', ['TaggedReferenceProduct'])
 
-        # Adding model 'TaggedReferenceService'
-        db.create_table(u'ecobasa_taggedreferenceservice', (
+        # Adding model 'TaggedReferenceSkill'
+        db.create_table(u'ecobasa_taggedreferenceskill', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('tag', self.gf('django.db.models.fields.related.ForeignKey')(related_name=u'ecobasa_taggedreferenceservice_items', to=orm['taggit.Tag'])),
+            ('tag', self.gf('django.db.models.fields.related.ForeignKey')(related_name=u'ecobasa_taggedreferenceskill_items', to=orm['taggit.Tag'])),
             ('content_object', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['ecobasa.Reference'])),
         ))
-        db.send_create_signal(u'ecobasa', ['TaggedReferenceService'])
+        db.send_create_signal(u'ecobasa', ['TaggedReferenceSkill'])
 
         # Adding model 'Reference'
         db.create_table(u'ecobasa_reference', (
@@ -39,6 +39,7 @@ class Migration(SchemaMigration):
             ('receiver_pioneer', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name=u'ecobasa_reference_receiver_pioneer', null=True, to=orm['auth.User'])),
             ('receiver_community', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name=u'ecobasa_reference_receiver_community', null=True, to=orm['cosinnus.CosinnusGroup'])),
             ('date', self.gf('django.db.models.fields.DateField')(auto_now=True, blank=True)),
+            ('rating', self.gf('django.db.models.fields.BooleanField')(default=False)),
             ('text', self.gf('django.db.models.fields.TextField')()),
         ))
         db.send_create_signal(u'ecobasa', ['Reference'])
@@ -57,14 +58,14 @@ class Migration(SchemaMigration):
         # Removing unique constraint on 'Reference', fields ['giver', 'receiver_pioneer']
         db.delete_unique(u'ecobasa_reference', ['giver_id', 'receiver_pioneer_id'])
 
-        # Deleting model 'TaggedReferenceSkill'
-        db.delete_table(u'ecobasa_taggedreferenceskill')
+        # Deleting model 'TaggedReferenceService'
+        db.delete_table(u'ecobasa_taggedreferenceservice')
 
         # Deleting model 'TaggedReferenceProduct'
         db.delete_table(u'ecobasa_taggedreferenceproduct')
 
-        # Deleting model 'TaggedReferenceService'
-        db.delete_table(u'ecobasa_taggedreferenceservice')
+        # Deleting model 'TaggedReferenceSkill'
+        db.delete_table(u'ecobasa_taggedreferenceskill')
 
         # Deleting model 'Reference'
         db.delete_table(u'ecobasa_reference')
@@ -130,7 +131,9 @@ class Migration(SchemaMigration):
         },
         u'cosinnus.cosinnusgroup': {
             'Meta': {'ordering': "(u'name',)", 'object_name': 'CosinnusGroup'},
+            'description': ('tinymce.models.HTMLField', [], {'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'media_tag': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['cosinnus.TagObject']", 'unique': 'True', 'null': 'True', 'on_delete': 'models.PROTECT', 'blank': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'public': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50', 'blank': 'True'}),
@@ -144,6 +147,21 @@ class Migration(SchemaMigration):
             'status': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '0', 'db_index': 'True'}),
             'user': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'cosinnus_memberships'", 'to': u"orm['auth.User']"})
         },
+        u'cosinnus.tagobject': {
+            'Meta': {'object_name': 'TagObject'},
+            'group': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'+'", 'null': 'True', 'to': u"orm['cosinnus.CosinnusGroup']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location_place': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '255', 'blank': 'True'}),
+            'people_name': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '255', 'blank': 'True'}),
+            'persons': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "u'+'", 'null': 'True', 'symmetrical': 'False', 'to': u"orm['auth.User']"}),
+            'public': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'visibility': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '1', 'blank': 'True'})
+        },
+        u'ecobasa.caravan': {
+            'Meta': {'ordering': "(u'name',)", 'object_name': 'Caravan', '_ormbases': [u'cosinnus.CosinnusGroup']},
+            u'cosinnusgroup_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['cosinnus.CosinnusGroup']", 'unique': 'True', 'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
         u'ecobasa.ecobasacommunityprofile': {
             'Meta': {'object_name': 'EcobasaCommunityProfile'},
             'basic_brings_together': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
@@ -152,20 +170,25 @@ class Migration(SchemaMigration):
             'basic_membership_status': ('django.db.models.fields.CharField', [], {'default': "u'o'", 'max_length': '2', 'blank': 'True'}),
             'contact_city': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
             'contact_country': ('django.db.models.fields.CharField', [], {'default': "u'ZZ'", 'max_length': '2', 'blank': 'True'}),
-            'contact_lat': ('django.db.models.fields.FloatField', [], {'default': '0.0', 'blank': 'True'}),
-            'contact_lon': ('django.db.models.fields.FloatField', [], {'default': '0.0', 'blank': 'True'}),
+            'contact_location': ('osm_field.fields.OSMField', [], {'null': 'True', 'blank': 'True'}),
+            'contact_location_lat': ('osm_field.fields.LatitudeField', [], {'null': 'True', 'blank': 'True'}),
+            'contact_location_lon': ('osm_field.fields.LongitudeField', [], {'null': 'True', 'blank': 'True'}),
             'contact_show': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'contact_street': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
             'contact_telephone': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
             'contact_zipcode': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
             'group': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "u'profile'", 'unique': 'True', 'to': u"orm['cosinnus.CosinnusGroup']"}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             'offers_learning_seminars': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             'offers_workshop_spaces': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'video': ('django.db.models.fields.URLField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
             'visitors_accommodation': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             'visitors_num': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0', 'blank': 'True'}),
+            'website': ('django.db.models.fields.URLField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
             'wishlist_materials': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'wishlist_projects': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             'wishlist_special_needs': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             'wishlist_tools': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
         },
@@ -208,6 +231,7 @@ class Migration(SchemaMigration):
             'date': ('django.db.models.fields.DateField', [], {'auto_now': 'True', 'blank': 'True'}),
             'giver': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'ecobasa_reference_giver'", 'to': u"orm['auth.User']"}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'rating': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'receiver_community': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "u'ecobasa_reference_receiver_community'", 'null': 'True', 'to': u"orm['cosinnus.CosinnusGroup']"}),
             'receiver_pioneer': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "u'ecobasa_reference_receiver_pioneer'", 'null': 'True', 'to': u"orm['auth.User']"}),
             'text': ('django.db.models.fields.TextField', [], {})
@@ -231,6 +255,12 @@ class Migration(SchemaMigration):
             'content_object': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['ecobasa.EcobasaUserProfile']"}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'tag': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'ecobasa_taggedinterest_items'", 'to': u"orm['taggit.Tag']"})
+        },
+        u'ecobasa.taggedoffers': {
+            'Meta': {'object_name': 'TaggedOffers'},
+            'content_object': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['ecobasa.Caravan']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'tag': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'ecobasa_taggedoffers_items'", 'to': u"orm['taggit.Tag']"})
         },
         u'ecobasa.taggedofferscreation': {
             'Meta': {'object_name': 'TaggedOffersCreation'},
@@ -279,6 +309,12 @@ class Migration(SchemaMigration):
             'content_object': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['ecobasa.EcobasaUserProfile']"}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'tag': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'ecobasa_taggedskill_items'", 'to': u"orm['taggit.Tag']"})
+        },
+        u'ecobasa.taggedwishskill': {
+            'Meta': {'object_name': 'TaggedWishSkill'},
+            'content_object': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['ecobasa.EcobasaCommunityProfile']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'tag': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'ecobasa_taggedwishskill_items'", 'to': u"orm['taggit.Tag']"})
         },
         'filer.file': {
             'Meta': {'object_name': 'File'},
@@ -329,6 +365,13 @@ class Migration(SchemaMigration):
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
             'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '100'})
+        },
+        u'taggit.taggeditem': {
+            'Meta': {'object_name': 'TaggedItem'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'taggit_taggeditem_tagged_items'", 'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'object_id': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'}),
+            'tag': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'taggit_taggeditem_items'", 'to': u"orm['taggit.Tag']"})
         }
     }
 
