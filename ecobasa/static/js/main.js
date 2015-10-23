fb8f77e669bc5716e064a51c643b32f9e828c607
Arne Bollinger <arne@ecobasa.org>
2015-10-23 17:25:22 +0200

    blog note detail parallax

--- ecobasa/static/js/main.js
+++ ecobasa/static/js/main.js
@@ -7,6 +7,38 @@ $(window).load( function() {
 });
 
 $(document).ready(function() {
+	// Cache the Window object
+	$window = $(window);
+
+	$('section[data-type="background"]').each(function(){
+      var $bgobj = $(this); // assigning the object
+      
+      $(window).scroll(function() {
+            var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+            
+            // Put together our final background position
+            var coords = '50% '+ yPos + 'px';
+
+            // Move the background
+            $bgobj.css({ backgroundPosition: coords });
+        }); 
+    });
+
+    $('div[data-type="text"]').each(function(){
+      var $bgobj = $(this); // assigning the object
+      
+      $(window).scroll(function() {
+            var yPos = ($window.scrollTop() / $bgobj.data('speed')); 
+            
+            // Put together our final background position
+            var coords = 400 + yPos + 'px';
+
+            // Move the background
+            $bgobj.css({ top: coords });
+            console.log(coords);
+        }); 
+    });
+
 	$('.email').tooltip();
 	
 	$('#accordion_registration .collapse').on('shown.bs.collapse', function () {
