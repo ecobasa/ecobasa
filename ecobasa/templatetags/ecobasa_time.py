09da95e689c44d536e437b836749c12d19f33793
ArneBo <arne@ecobasa.org>
2017-09-11 02:24:49 +0200

    dashboard translation de

--- ecobasa/templatetags/ecobasa_time.py
+++ ecobasa/templatetags/ecobasa_time.py
@@ -16,3 +16,15 @@ def time_of_day():
 		return 'afternoon'
 	else:
 		return 'evening'
+
+@register.simple_tag
+def tageszeit():
+	import datetime, pytz
+	from django.conf import settings
+	cur_time = datetime.datetime.now(tz=pytz.timezone(str(settings.TIME_ZONE)))
+	if cur_time.hour < 12:
+		return 'Morgen'
+	elif 12 <= cur_time.hour < 18:
+		return 'Tag'
+	else:
+		return 'Abend'
